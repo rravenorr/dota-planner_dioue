48825a08c854dbc6896e53deb3699900e5f7124e
Joe Marvin Tabañera <joemarvin.dev@gmail.com>
2024-01-22 18:38:31 +0800

    Saved

--- app/auth.py
+++ app/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "login"
+    return render_template('login.html', boolean = True )
 
 @auth.route('/logout')
 def logout():
