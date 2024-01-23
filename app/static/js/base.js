be09b6136cadf44268b76044a09195705ec289ba
Joe Marvin Tabañera <joemarvin.dev@gmail.com>
2024-01-23 18:40:24 +0800

    Added css styling for navigation

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -2,9 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const navbar = document.querySelector(".navbar");
   
     window.onscroll = function () {
-      console.log("Scrolling...");
-      console.log("Navbar:", navbar);
-  
+
       if (navbar && window.scrollY > 0) {
         navbar.classList.add("dark-bg");
       } else if (navbar) {
@@ -12,4 +10,18 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     };
   });
-  
+
+var homeButton = document.querySelector('#home');
+var loginButton = document.querySelector('#login');
+var registerButton = document.querySelector('#register');
+/* Checking the current route */
+var currentRoute = window.location.pathname;
+
+// Check the current route
+if (currentRoute === "/" || currentRoute === "/home" || currentRoute === "/index"){
+  homeButton.classList.add('highlighted');
+} else if (currentRoute === "/login"){
+  loginButton.classList.add("highlighted");
+} else if (currentRoute === "/register"){
+  registerButton.classList.add("highlighted");
+}
